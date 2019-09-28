32034b69d69718b06221f01741c2a9665bba7f8c
kurisd <tnatesco@gmail.com>
2019-09-28 12:01:37 +0200

    add rm and fix warning
    fixed all g++ warning and added rm and rm -rf capabilities
    changed if menu for switch case form

--- terminal.cpp
+++ terminal.cpp
@@ -9,6 +9,8 @@ Ls listazas
 Mkdir konyvar letrehozasa
 Cd belepes
 Cd.. kilepes
+rm -rf mappa torol mappat es a benne levoket is
+rm ha ures mappa torol
 */
 
 //globalis valzotok
@@ -22,17 +24,18 @@ void listazas(){
     for (int j = 0; j < hasznal; j++){
         string seged="";
         bool pont =false;
-        for (int i=0;i<mappanevek[j].length();i++){        //kereses.
+        int e =mappanevek[j].length();
+        for (int i=0;i<e;i++){        //kereses.
             seged=seged+mappanevek[j][i];
             if(seged==akt){
-                for (int l=i+1;l<mappanevek[j].length();l++){
+                for (int l=i+1;l<e;l++){
                     if (mappanevek[j][l]=='/'){
                         pont=true;
                     }
                 }
                 if(pont==false){
                     cout<<"[";
-                for(int k=(seged.length());k<(mappanevek[j].length());k++){             
+                for(int k=(seged.length());k<e;k++){             
                     //csak az aktualis mappa neve irja elotagokat nem
                     cout<<mappanevek[j][k];
                 }
@@ -56,7 +59,7 @@ void mappa(string nev){
                 delete[] mappanevek;
                 mappanevek = mappanevek2;
             }
-                 //almappak. elvalasztas
+                 //almappak / elvalasztas
             mappanevek[hasznal] = (akt+nev);
             (hasznal)++;
 }
@@ -81,33 +84,104 @@ void cdki(){
             }
         }
     }else{
-        cout<<"gyökér könyvtár\n";
+        cout<<"Root directory\n";
     }
 
    // cout<< "["<<akt<<"[\n";
     //torlendo csak seged!!!
 }
 
+//force torles
+void fdelet(string resz){
+    bool name = false;
+    for (int j = 0; j < hasznal; j++){
+        string seged="";
+        string seged2="";
+        int e =mappanevek[j].length();
+        for (int i=0;i<e;i++){
+            seged=seged+mappanevek[j][i];
+            if(seged==akt){
+                for (int l=i+1;l<e;l++){
+                    seged2=seged2+mappanevek[j][l];
+                    if (seged2==resz){
+                        mappanevek[j]="";       //memoria szemet
+                        name=true;
+                    }
+                }    
+            }
+        }
+    }
+    if (name==false){
+        cout << "Directory does not exist\n";
+    }
+}
+
+//torles
+void del(string resz){
+    bool name = false;      //folder name not found
+    bool emp = false;       //if folder exist
+    int ext;                //folder space in dy
+    for (int j = 0; j < hasznal; j++){
+        string seged="";
+        string seged2="";
+        int e =mappanevek[j].length();
+        for (int i=0;i<e;i++){
+            seged=seged+mappanevek[j][i];
+            if(seged==akt){
+                for (int l=i+1;l<e;l++){
+                    seged2=seged2+mappanevek[j][l];
+                    if(mappanevek[j][l]=='/'){
+                        emp = true;
+                    }
+                    if (seged2==resz){
+                        name=true;
+                        ext=j;
+                    }
+                }    
+            }
+        }
+    }
+    if (name==false){
+        cout << "Directory does not exist\n";
+    }
+    if(emp==false and name==true){
+        mappanevek[ext]="";             //memoria szemet
+    }
+    if(emp==true and name == true){
+        cout<< "Directory is not empty\n";
+    }
+}
+
 //main!!
 int main(){
 string be;
 do{
-string resz;
+string masodik;
 cout<<"\033[1;32madmin\033[0m:"<<"\033[1;34m~"<<akt<<"\033[0m$";     //zold szin32    
 getline (cin,be);
 
 //darabolas
 int count =0;
 string elso ="";
-for (int i=0;i<be.length();i++){
-
+string harmadik;
+int e = be.length();
+for (int i=0;i<e;i++){
     if (isspace(be[i])){
-            count=1;
+            count++;
     }else{
-        if (count==1){
-        resz=resz+(be[i]);
-        }else{
-        elso=elso+(be[i]);
+        switch (count)
+        {
+        case 1:
+           masodik=masodik+(be[i]);
+            break;
+        case 2:
+            harmadik=harmadik+(be[i]);
+            break;
+        case 0: 
+            elso=elso+(be[i]);
+            break;
+        default:
+            break;
         }
     }
 }
@@ -116,18 +190,24 @@ for (int i=0;i<be.length();i++){
 if (elso =="ls"){
     listazas();
 }
+if(elso == "rm" and masodik== "-rf"){
+    fdelet(harmadik);
+}
+if(elso=="rm"){
+    del(masodik);
+}
 if(elso=="mkdir"){
-    mappa(resz);
+    mappa(masodik);
 }
-if(elso=="cd" and resz!=".."){
-    cdbe(resz);
+if(elso=="cd" and masodik!=".."){
+    cdbe(masodik);
 }
-if(elso=="cd" and resz==".."){
+if(elso=="cd" and masodik==".."){
     cdki();
 }
-}while (be!="kilep");
+}while (be!="exit");
 
-//dinamikus adat torlese
+//dynamic array delete
 delete[] mappanevek;
     return 0;
 }
